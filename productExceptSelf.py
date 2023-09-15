8c036a1cc74d05819a472f73e42d7aeda7f00cda
hamlim0124 <37390843+hamlim0124@users.noreply.github.com>
2023-09-15 10:42:25 +0900

    productExceptSelf

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,10 +1,17 @@
 from typing import List
 
 def productExceptSelf(nums: List[int]) -> List[int]:
-    ans = []
-    for n in nums:
-        product = nums
+    res = [1] * (len(nums))
 
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) -1 , -1 ,-1 ):
+        res[i] *= postfix
+        postfix *= nums[i]
+    return res
 
 
 input = [1,2,3,4]
