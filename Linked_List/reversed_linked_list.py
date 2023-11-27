faf8394b82e9d28bc34684d36fa3eddc446ab9d0
deepalgorithm <hamin.lim@gmail.com>
2023-11-27 13:42:11 +0900

    reversed linked list

---
+++ Linked_List/reversed_linked_list.py
@@ -0,0 +1,10 @@
+def reversedLinkedList(head):
+    prev, curr = None, head
+
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+
+    return prev
