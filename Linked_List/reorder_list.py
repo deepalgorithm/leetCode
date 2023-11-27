1575769519402c72504d66dd0650c84f4864b38a
deepalgorithm <hamin.lim@gmail.com>
2023-11-27 14:39:00 +0900

    merge two sorted lists, reoder list

---
+++ Linked_List/reorder_list.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next =tmp1
+            first, second = tmp1, tmp2
