e046e02d1d5a65eeb9bbc128369b95162ec4cc7f
deepalgorithm <hamin.lim@gmail.com>
2023-11-28 14:10:42 +0900

    Search Rotated Sorted Array

---
+++ Binary_Search/search_in_rotated_sorted_array.py
@@ -0,0 +1,20 @@
+def search(nums, target):
+    l, r = 0, len(nums) -1
+
+    while l <= r:
+        m = (l+r) // 2
+        if target == nums[m]:
+            return m
+        if nums[l] <= nums[m]:
+            if target > nums[m] or target < nums[l]:
+                l = m +1
+            else:
+                r = m -1
+        else:
+            if target < nums[m] or target > nums[r]:
+                r = m -1
+            else:
+                l = m +1
+    return -1
+
+print(search([4,5,6,7,0,1,2], 0))
