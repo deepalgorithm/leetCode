53b432d2cabf3b1f023b2daef1e0e91ddc62c7da
deepalgorithm <hamin.lim@gmail.com>
2023-12-04 14:14:15 +0900

    좌표정렬

---
+++ algo_practice/좌표정렬.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+array = []
+
+for _ in range(n):
+    x ,y = map(int, input().split(' '))
+    array.append((x,y))
+array = sorted(array)
+
+for i in array:
+    print(i[0], i[1])
