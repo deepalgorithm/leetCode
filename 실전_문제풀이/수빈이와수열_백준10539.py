d6ec7abfa171f599cba96d0517a0b4ca8ad2ce97
deepalgorithm <hamin.lim@gmail.com>
2024-01-19 10:37:33 +0900

    이름궁합테스트_백준17269

--- 실전_문제풀이/수빈이와수열_백준10539.py
+++ 실전_문제풀이/수빈이와수열_백준10539.py
@@ -3,7 +3,10 @@ n, array = int(input()), list(map(int, input().split()))
 result = [array[0]]
 
 for i in range(1, n):
-    print(array[i] * (i +1), sum(result))
+    #print(array[i] * (i +1), sum(result))
     result.append(array[i] * (i+1) - sum(result))
 
-print(result)
+#print(result)
+    
+for i in result:
+    print(i, end=" ")
