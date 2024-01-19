f793b0f77dddc5e393d984192aee682a1bef088f
deepalgorithm <hamin.lim@gmail.com>
2024-01-19 10:12:48 +0900

    수빈이와수열_백준10539

---
+++ 실전_문제풀이/수빈이와수열_백준10539.py
@@ -0,0 +1,9 @@
+n, array = int(input()), list(map(int, input().split()))
+
+result = [array[0]]
+
+for i in range(1, n):
+    print(array[i] * (i +1), sum(result))
+    result.append(array[i] * (i+1) - sum(result))
+
+print(result)
