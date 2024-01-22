5e715966ae706e37d324acc33f3a469ce2a25ad1
deepalgorithm <hamin.lim@gmail.com>
2024-01-22 11:00:33 +0900

    캔디워_백준9037

---
+++ 실전_문제풀이/캔디워_백준9037.py
@@ -0,0 +1,29 @@
+def check(N, candy):
+    for i in range(N):
+        if candy[i] % 2 == 1:
+            candy[i] += 1
+    return len(set(candy)) == 1
+
+def teacher(N, candy):
+    tmp_list = [0 for i in range(N)]
+    for idx in range(N):
+        if candy[idx] % 2:
+            candy[idx] += 1
+        candy[idx] //= 2
+        tmp_list[(idx+1) % N] = candy[idx]
+
+    for idx in range(N):
+        candy[idx] += tmp_list[idx]
+
+    return candy
+
+def process():
+    N, candy = int(input()), list(map(int, input().split()))
+    count = 0 
+    while not check(N, candy):
+        count += 1
+        candy = teacher(N, candy)
+    print(count)
+
+for i in range(int(input())):
+    process()
