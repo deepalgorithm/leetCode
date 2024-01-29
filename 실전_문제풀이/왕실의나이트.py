3ff04c97975263d82ec1b46fc832de6de2702973
deepalgorithm <hamin.lim@gmail.com>
2024-01-29 22:49:57 +0900

    왕실의 나이트

---
+++ 실전_문제풀이/왕실의나이트.py
@@ -0,0 +1,18 @@
+input_data = input()
+
+# 현재 나이트의 위치
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
+
+result = 0
+for step in steps:
+    #이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    #해당 위치로 이동이 가능하면 카운트 증가
+    if next_row >=  1 and next_column >= 1 and next_row <= 8 and next_column <= 8:
+        result += 1
+
+print(result)
