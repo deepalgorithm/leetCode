b71e5088a224cb07fb5383127bfbcc44bdec6507
deepalgorithm <hamin.lim@gmail.com>
2024-02-19 14:46:21 +0900

    문자열재정렬

---
+++ 이코테_알고리즘_유형별_기출문제/구현/문자열재정렬.py
@@ -0,0 +1,17 @@
+n = input()
+
+result = []
+value = 0
+
+for x in n:
+    if x.isalpha():
+        result.append(x)
+    else:
+        value += int(x)
+
+result.sort()
+
+result.append(str(value))
+
+print(''.join(result))
+
