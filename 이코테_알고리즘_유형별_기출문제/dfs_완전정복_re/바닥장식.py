7a04474bda0e349c8bc91a1c385ad05b1f804861
deepalgorithm <hamin.lim@gmail.com>
2024-02-27 09:21:05 +0900

    바닥장식_re

--- 이코테_알고리즘_유형별_기출문제/dfs_완전정복_re/바닥장식.py
+++ 이코테_알고리즘_유형별_기출문제/dfs_완전정복_re/바닥장식.py
@@ -2,6 +2,18 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
+#가로 또는 세로만 확인
+def dfs(y,x):
+    global map_, visited
+    visited[y][x] = True
+
+    if map_[y][x] == '-' and map_[y][x + 1] == '-':
+        dfs(y, x + 1)
+    elif map_[y][x] == '|' and map_[y+1][x] == '|':
+        dfs(y + 1, x)
+
+
+
 # 0. 입력 조건 설정
 N, M = map(int, input().split())
 MAX = 50 + 10
@@ -13,7 +25,12 @@ answer = 0
 for i in range(1, N + 1):
     row = input()
     for j in range(1, M + 1):
-        map_[i][j] = row(j -1)
+        map_[i][j] = row[j-1]
 # 2. dfs 실행
-
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        if not visited[i][j]:
+            dfs(i, j)
+            answer += 1
 # 3. 출력
+print(answer)
