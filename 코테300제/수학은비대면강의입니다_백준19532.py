4a38f88ae8ec6b17a7902889ea21aa4cf3898781
Hamin <hamin.lim@gmail.com>
2025-03-12 09:53:40 +0900

    brute_force_백준19532.py

---
+++ 코테300제/수학은비대면강의입니다_백준19532.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/19532
+
+# Brute Force
+
+
+a, b, c, d, e, f = map(int, input().split())
+
+if b == 0:
+    x = c // a
+    y = (f -d * x) // e
+    print(x, y)
+else: 
+    for x in  range( -999, 1000):
+        if (c - a * x) % b ==0:
+            y = (c -a * x) // b
+            if d * x + e * y == f:
+                print(x, y)
+                break
